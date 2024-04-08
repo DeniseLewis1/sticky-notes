f5acceb6accdfeeb2a88e7c0dd0eb34e6d31d363
Denise <deniselewis1120@gmail.com>
2024-04-07 19:06:17 -0700

    Minor layout change

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,28 +8,32 @@ const Header = ({ addNote, searchText, searchNote, categories, category, handleC
     <header>
       <h1 className="app-header__title">Sticky Notes</h1>
       <aside className="app-header__controls">
-        <button onClick={addNote} className="add-new">
-          + New Note
-        </button>
-        <input
-          type="text"
-          placeholder="Type here to search..."
-          className="search"
-          value={searchText}
-          onChange={searchInput}
-        />
+        <div className="app-header__left">
+          <button onClick={addNote} className="add-new">
+            + New Note
+          </button>
+          <input
+            type="text"
+            placeholder="Type here to search..."
+            className="search"
+            value={searchText}
+            onChange={searchInput}
+          />
+        </div>
+        <div className="app-header__right">
         <form className="category-filter">
-          <Filter />
-          <label for="category">Filter Notes:</label>
-          <select 
-            id="category" 
-            name="category" 
-            value={category} 
-            onChange={handleCategoryChange}
-          >
-            {categories.map(category => <option value={category} key={category}>{category}</option>)}
-          </select>
-          </form>
+            <Filter />
+            <label for="category">Filter Notes:</label>
+            <select 
+              id="category" 
+              name="category" 
+              value={category} 
+              onChange={handleCategoryChange}
+            >
+              {categories.map(category => <option value={category} key={category}>{category}</option>)}
+            </select>
+            </form>
+          </div>
       </aside>
     </header>
   );
